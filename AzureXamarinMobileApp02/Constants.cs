9bc04ba5bf131aa4b19e47b1568d6710ff47b31a
Lutz Brandt <lutz.brandt@fairmas.com>
2017-07-31 16:32:54 +0200

    Add project files.

---
+++ AzureXamarinMobileApp02/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AzureXamarinMobileApp02
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://azurexamarinmobileapp02.azurewebsites.net";
+	}
+}
+
